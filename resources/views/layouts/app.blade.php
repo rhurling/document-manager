2bcd25121c221cf6976b65a9d67a7674856ca991
Rouven Hurling <rouven@rhurling.de>
2016-01-29 19:51:24 +0100

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                Laravel
+                Document Management
             </a>
         </div>
 
